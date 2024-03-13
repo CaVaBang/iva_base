13a550c4e2a3af78ac55516b4c634c2358ee8644
CaVaBang <musmag7@mail.ru>
2024-03-13 20:20:14 +0300

    new

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def home():
     return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
