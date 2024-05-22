d4b65e66b7b438a2cdd12716f24d556b919bf4c9
m.magomedov <m.magomedov@yadro.com>
2024-05-22 16:53:32 +0300

    mysql

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+import pymysql.cursors
+
+
 
 app = Flask(__name__)
 
@@ -27,3 +32,31 @@ def blog():
 @app.route('/contact.html')
 def contact():
     return render_template('contact.html')
+
+# Настройки подключения к базе данных
+connection = pymysql.connect(
+    host='CaVaBang.mysql.pythonanywhere-services.com',
+    user='CaVaBang',
+    password='2PacNotorious',
+    database='CaVaBang$Clients',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    phone = request.form['phone']
+    email = request.form['email']
+    message = request.form['message']
+
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO bookings (name, phone, email, message) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql, (name, phone, email, message))
+        connection.commit()
+
+    return redirect(url_for('thank_you'))
+
+
+@app.route('/thank_you')
+def thank_you():
+    return 'Спасибо! Ваша заявка принята.'
