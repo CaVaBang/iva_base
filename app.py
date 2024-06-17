7f71b10b011237a655f2989e57d26e9b3de5db78
m.magomedov <m.magomedov@yadro.com>
2024-06-17 17:39:29 +0300

    remove db

--- app.py
+++ app.py
@@ -38,57 +38,57 @@ def contact():
     return render_template('contact.html')
 
 
-# Подключения к базе данных
-connection = pymysql.connect(
-    host='CaVaBang.mysql.pythonanywhere-services.com',
-    user='CaVaBang',
-    password='2PacNotorious',
-    database='CaVaBang$Clients',
-    cursorclass=pymysql.cursors.DictCursor
-)
-
-
-@app.route('/submit_form', methods=['POST'])
-def submit_form():
-    name = request.form['name']
-    phone = request.form['phone']
-    email = request.form['email']
-    message = request.form['message']
-    check_in = request.form['check_in']
-    check_out = request.form['check_out']
-    guests = request.form['guests']
-
-    with connection.cursor() as cursor:
-        sql = """INSERT INTO bookings (name, phone, email, message, check_in, check_out, guests)
-                 VALUES (%s, %s, %s, %s, %s, %s, %s)"""
-        cursor.execute(sql, (name, phone, email, message, check_in, check_out, guests))
-        connection.commit()
-
-    # Отправка email уведомления
-    admin_email = 'musnigga@mail.ru'
-    subject = 'Новая бронь'
-    body = f'Имя: {name}\nТелефон: {phone}\nEmail: {email}\nСообщение: {message}\nДата заезда: {check_in}\nДата выезда: {check_out}\nГости: {guests}'
-    send_email(subject, body, admin_email)
-
-    return '', 200
-
-
-def send_email(subject, body, to_email):
-    from_email = 'test_the_iva_site@mail.ru'
-    password = '10923874q'
-
-    # Настройка MIME сообщения
-    msg = MIMEMultipart()
-    msg['From'] = from_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
-
-    msg.attach(MIMEText(body, 'plain'))
-
-    # Настройка SMTP сервера
-    server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-    server.login(from_email, password)
-    text = msg.as_string()
-    server.sendmail(from_email, to_email, text)
-    server.quit()
+# # Подключения к базе данных
+# connection = pymysql.connect(
+#     host='CaVaBang.mysql.pythonanywhere-services.com',
+#     user='CaVaBang',
+#     password='2PacNotorious',
+#     database='CaVaBang$Clients',
+#     cursorclass=pymysql.cursors.DictCursor
+# )
+#
+#
+# @app.route('/submit_form', methods=['POST'])
+# def submit_form():
+#     name = request.form['name']
+#     phone = request.form['phone']
+#     email = request.form['email']
+#     message = request.form['message']
+#     check_in = request.form['check_in']
+#     check_out = request.form['check_out']
+#     guests = request.form['guests']
+#
+#     with connection.cursor() as cursor:
+#         sql = """INSERT INTO bookings (name, phone, email, message, check_in, check_out, guests)
+#                  VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+#         cursor.execute(sql, (name, phone, email, message, check_in, check_out, guests))
+#         connection.commit()
+#
+#     # Отправка email уведомления
+#     admin_email = 'musnigga@mail.ru'
+#     subject = 'Новая бронь'
+#     body = f'Имя: {name}\nТелефон: {phone}\nEmail: {email}\nСообщение: {message}\nДата заезда: {check_in}\nДата выезда: {check_out}\nГости: {guests}'
+#     send_email(subject, body, admin_email)
+#
+#     return '', 200
+#
+#
+# def send_email(subject, body, to_email):
+#     from_email = 'test_the_iva_site@mail.ru'
+#     password = '10923874q'
+#
+#     # Настройка MIME сообщения
+#     msg = MIMEMultipart()
+#     msg['From'] = from_email
+#     msg['To'] = to_email
+#     msg['Subject'] = subject
+#
+#     msg.attach(MIMEText(body, 'plain'))
+#
+#     # Настройка SMTP сервера
+#     server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
+#     server.login(from_email, password)
+#     text = msg.as_string()
+#     server.sendmail(from_email, to_email, text)
+#     server.quit()
 
