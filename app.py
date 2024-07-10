84b03b29764038a43173165bb88208aab0f96bb1
m.magomedov <m.magomedov@yadro.com>
2024-07-10 13:56:32 +0300

    fix utf8

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ connection = pymysql.connect(
     user='cj34869_bookings',
     password='igYz6qO2UP@!',
     database='cj34869_bookings',
+    charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor
 )
 
@@ -66,24 +67,21 @@ def submit_form():
         cursor.execute(sql, (name, phone, email, message, check_in, check_out, guests))
         connection.commit()
 
-    #send_email(name, phone, email, message, check_in, check_out, guests)
     send_email(name, phone, email, message, check_in, check_out, guests)
-
 
     return '', 200
 
 
 def send_email(name, phone, email, message, check_in, check_out, guests):
-    # Настройки для подключения к SMTP серверу Timeweb
     smtp_server = "smtp.timeweb.ru"
     smtp_port = 465
-    smtp_login = "cj34869@cj34869.tw1.ru"  # замените на ваш email
-    smtp_password = "igYz6qO2UP@!"  # замените на ваш пароль
+    smtp_login = "cj34869@cj34869.tw1.ru"
+    smtp_password = "igYz6qO2UP@!"
 
     # Создание сообщения
     msg = MIMEMultipart()
     msg['From'] = smtp_login
-    msg['To'] = 'musnigga@mail.ru'  # можно отправить самому себе или указать нужный email
+    msg['To'] = 'musnigga@mail.ru'
     msg['Subject'] = "Новая запись"
 
     # Текст сообщения
@@ -101,7 +99,6 @@ def send_email(name, phone, email, message, check_in, check_out, guests):
     msg.attach(MIMEText(body, 'plain'))
 
     try:
-        # Установка соединения с SMTP сервером
         server = smtplib.SMTP_SSL(smtp_server, smtp_port)
         server.login(smtp_login, smtp_password)
         server.send_message(msg)
@@ -109,30 +106,3 @@ def send_email(name, phone, email, message, check_in, check_out, guests):
         print("Email sent successfully")
     except Exception as e:
         print(f"Failed to send email: {e}")
-
-# def send_email(name, phone, email, message, check_in, check_out, guests):
-#     smtp_server = "smtp.timeweb.ru"
-#     port = 25  # Или 2525 для STARTTLS
-#     sender_email = "cj34869@cj34869.tw1.ru"
-#     sender_password = "igYz6qO2UP@!"
-#     admin_email = "musnigga@mail.ru"
-#
-#     # Создание MIME сообщения
-#     msg = MIMEMultipart()
-#     msg['From'] = sender_email
-#     msg['To'] = admin_email
-#     msg['Subject'] = "New Booking Received"
-#     body = (f"New booking details:\n\n"
-#             f"Name: {name}\n"
-#             f"Phone: {phone}\n"
-#             f"Email: {email}\n"
-#             f"Message: {message}\n"
-#             f"Check-in: {check_in}\n"
-#             f"Check-out: {check_out}\n"
-#             f"Guests: {guests}\n")
-#     msg.attach(MIMEText(body, 'plain'))
-#
-#     with smtplib.SMTP(smtp_server, port) as server:
-#         server.starttls()
-#         server.login(sender_email, sender_password)
-#         server.sendmail(sender_email, admin_email, msg.as_string())
